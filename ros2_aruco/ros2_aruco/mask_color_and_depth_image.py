0db5b6f8f24742657f7ec592a49a46fa0a79b3d4
Andy Chien <r960411@gmail.com>
2023-07-08 15:29:18 +0800

    seg mask using model

--- ros2_aruco/ros2_aruco/mask_color_and_depth_image.py
+++ ros2_aruco/ros2_aruco/mask_color_and_depth_image.py
@@ -42,6 +42,8 @@ from ros2_aruco_interfaces.msg import ArucoMarkers
 from ros2_aruco_interfaces.srv import GetMaskImage
 from math import *
 import copy
+import open3d as o3d
+import trimesh
 
 DEPTH_THRESHOLD = 8
 DRAW_MARKER_POSE = False
@@ -63,6 +65,39 @@ class ArucoImageMasker:
         self.depth_masks = dict()
         self.empty_depth_img = dict()
         self.corners_depth = dict()
+        mesh_file = "/home/andy/packing_ws/install/mr_description/share/mr_description/meshes/black_box_bigger.stl"
+        box_mesh = o3d.io.read_triangle_mesh(mesh_file)
+        self.box_mesh_trimesh = trimesh.load_mesh(mesh_file)
+        print(self.box_mesh_trimesh.bounds)
+        print(self.box_mesh_trimesh.center_mass)
+        print(self.box_mesh_trimesh.centroid)
+        o3d.visualization.draw_geometries([box_mesh], window_name='Open3D', width=1920, height=1080, \
+            left=50, top=50, point_show_normal=False, mesh_show_wireframe=False, mesh_show_back_face=False)
+        vis = o3d.visualization.Visualizer()
+        # Create a window, name it and scale it
+        vis.create_window(window_name='Bunny Visualize', width=800, height=600)
+
+        # Add the voxel grid to the visualizer
+        vis.add_geometry(box_mesh)
+
+        # We run the visualizater
+        vis.run()
+        # Once the visualizer is closed destroy the window and clean up
+        vis.destroy_window()
+        self.box_voxel = o3d.geometry.VoxelGrid.create_from_triangle_mesh(box_mesh, 0.001)
+        o3d.visualization.draw_geometries([self.box_voxel], window_name='Open3D', width=1920, height=1080, \
+            left=50, top=50, point_show_normal=False, mesh_show_wireframe=False, mesh_show_back_face=False)
+        voxel_list = self.box_voxel.get_voxels()
+        voxel_list = [np.array(voxel.grid_index, dtype=np.uint32) for voxel in voxel_list]
+        box_indx_size = np.max(voxel_list, axis=0) + 1
+        self.box_indx_size = box_indx_size
+        self.box_voxel_bool = [[[False] * box_indx_size[2]] * box_indx_size[1]] * box_indx_size[0]
+        # print('self.box_voxel.origin = {} \n self.box_voxel_bool = {}'.format(self.box_voxel.origin, self.box_voxel_bool))
+        for idx in voxel_list:
+            self.box_voxel_bool[idx[0]][idx[1]][idx[2]] = True
+        
+        # print('self.box_voxel.origin = {} \n self.box_voxel_bool = {}'.format(self.box_voxel.origin, self.box_voxel_bool))
+
 
     def set_empty_depth_img(self, depth_img, id):
         self.empty_depth_img[id] = copy.deepcopy(depth_img)
@@ -80,18 +115,24 @@ class ArucoImageMasker:
                 self.corners_depth[id].append(float(depth_img[corner[1], corner[0]]) / 1000.0)
         return True
 
-    def get_masked_img(self, rgb_img=None, depth_img=None, id=1, force_update=False):
+    def get_masked_img(self, marker_size, intrinsic_mat, distortion, rgb_img=None, depth_img=None, id=1, force_update=False, get_seg_mask=False):
         assert rgb_img.shape[:2] == depth_img.shape[:2]
+        self.marker_size = marker_size
+        self.intrinsic_mat = intrinsic_mat
+        self.distortion = distortion
         
         if not self.check_and_update_mask(rgb_img, depth_img, id, force_update):
             return None
 
-        if id in self.empty_depth_img:
-            rgb_mask, depth_mask, seg_mask_uint8 = self.compute_segmentation_mask(
-                depth_img, self.empty_depth_img[id], self.mask_corners[id])
+        rgb_mask = self.rgb_masks[id]
+        depth_mask = self.depth_masks[id]
+        # if id in self.empty_depth_img:
+            # rgb_seg_mask, depth_seg_mask, seg_mask_uint8 = self.compute_segmentation_mask(
+            #     depth_img, self.empty_depth_img[id], self.mask_corners[id])
+        if get_seg_mask and id in self.mask_corners and id in self.marker_corners:
+            seg_mask_uint8 = self.compute_uint8_segmentation_mask(
+                depth_img, self.mask_corners[id], self.marker_corners[id])
         else:
-            rgb_mask = self.rgb_masks[id]
-            depth_mask = self.depth_masks[id]
             bound = [np.min(self.mask_corners[id], axis=0), 
                      np.max(self.mask_corners[id], axis=0)]
             size = [bound[1][1] - bound[0][1], bound[1][0] - bound[0][0]]
@@ -118,9 +159,9 @@ class ArucoImageMasker:
         bounded_rgb_seg_mask = rgb_seg_mask[bound[0][1]:bound[1][1], bound[0][0]:bound[1][0]]
         bounded_depth_seg_mask = depth_seg_mask[bound[0][1]:bound[1][1], bound[0][0]:bound[1][0]]
         seg_mask_uint8 = np.zeros(sub_img.shape, dtype=np.uint8)
-        for i, row in enumerate(sub_img):
-            for j, x in enumerate(row):
-                if x > DEPTH_THRESHOLD:
+        for i, (sub_row, empty_row) in enumerate(zip(sub_img, bounded_empty_img)):
+            for j, (sub_x, empty_x) in enumerate(zip(sub_row, empty_row)):
+                if sub_x > DEPTH_THRESHOLD or (empty_x < DEPTH_THRESHOLD and sub_x < -1* DEPTH_THRESHOLD):
                     bounded_rgb_seg_mask[i][j] = rgb_mask_color
                     bounded_depth_seg_mask[i][j] = depth_mask_color
                     seg_mask_uint8[i][j] = np.iinfo(np.uint8).max
@@ -130,6 +171,71 @@ class ArucoImageMasker:
         rgb_seg_mask[bound[0][1]:bound[1][1], bound[0][0]:bound[1][0]] = bounded_rgb_seg_mask
         depth_seg_mask[bound[0][1]:bound[1][1], bound[0][0]:bound[1][0]] = bounded_depth_seg_mask
         return rgb_seg_mask, depth_seg_mask, seg_mask_uint8
+    
+    def compute_uint8_segmentation_mask(self, img, box_corners, marker_corners):
+        marker_poses = self.compute_marker_poses(marker_corners)
+        box_pos, box_q = self.compute_box_pose(marker_poses)
+        c_x = self.intrinsic_mat[0][2]
+        c_y = self.intrinsic_mat[1][2]
+        f_x = self.intrinsic_mat[0][0]
+        f_y = self.intrinsic_mat[1][1]
+        bound = [np.min(box_corners, axis=0), np.max(box_corners, axis=0)]
+        bounded_img = img[bound[0][1]:bound[1][1], bound[0][0]:bound[1][0]]
+        mat = np.identity(4)
+        mat[:3, :3] = qtn.as_rotation_matrix(box_q)
+        mat[:3, 3] = box_pos
+        mat = np.linalg.inv(mat)
+        seg_mask_uint8 = np.zeros(bounded_img.shape, dtype=np.uint8)
+        pos_list = []
+        indx_list = []
+        for i, row in enumerate(bounded_img):
+            for j, z in enumerate(row):
+                if z < DEPTH_THRESHOLD or z > 1100:
+                    continue
+                pixel_x = j + bound[0][0]
+                pixel_y = i + bound[0][1]
+                pos = np.array([0., 0., 0., 1.])
+                pos[0] = (pixel_x - c_x) * (float(z) / 1000.0) / f_x
+                pos[1] = (pixel_y - c_y) * (float(z) / 1000.0) / f_y
+                pos[2] = float(z + DEPTH_THRESHOLD) / 1000.0
+                pos = np.matmul(mat, pos)
+                pos_list.append(pos[:3])
+                indx_list.append([i, j])
+                # indx = self.box_voxel.get_voxel(pos.reshape((3,1)))
+                # if np.any(indx + 2 > self.box_indx_size) or \
+                #     np.any(indx + 2 < 0):
+                #     # print(pos, indx, self.box_indx_size)
+                #     continue
+                # # print('dfsdfdfdfdfdf')
+                # if not self.box_voxel_bool[indx[0]][indx[1]][indx[2]]:
+                #     seg_mask_uint8[i][j] = np.iinfo(np.uint8).max
+        
+        results = self.box_mesh_trimesh.contains(pos_list)
+        for indx, result in zip(indx_list, results):
+            if result: continue
+            seg_mask_uint8[indx[0]][indx[1]] = np.iinfo(np.uint8).max
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (DEPTH_THRESHOLD, DEPTH_THRESHOLD))
+        seg_mask_uint8 = cv2.dilate(cv2.erode(seg_mask_uint8, kernel), kernel)
+        return seg_mask_uint8
+                
+        
+
+    def compute_box_pose(self, marker_poses):
+        pos_list = []
+        for p in marker_poses:
+            pos = p.position
+            pos_list.append(np.array([pos.x, pos.y, pos.z]))
+
+        center_pos = np.mean(pos_list, axis=0)
+        v1 = pos_list[-1][:2] - pos_list[0][:2]
+        v2 = pos_list[1][:2] - pos_list[0][:2]
+        v = v1 if np.linalg.norm(v1) < np.linalg.norm(v2) else v2
+        q = qtn.from_rotation_vector(np.array([.0, .0, atan2(v[1], v[0])]))
+        qz = qtn.quaternion(0., 1., 0., 0.)
+        q *= qz
+
+        return center_pos, q
 
     def find_mask_corners(self, rgb_img, target_id):
         corners, ids = self.find_aruco(rgb_img)
@@ -149,10 +255,11 @@ class ArucoImageMasker:
             marker_corners.append(corners[i])
 
         order = [0]
-        for i, c in enumerate(mask_corners):
+        for _ in range(len(mask_corners) - 1):
+            c = mask_corners[order[-1]]
             d_min = 99999
             d_min_idx = 0
-            for j, cn in enumerate(mask_corners[1:]):
+            for j, cn in enumerate(mask_corners):
                 if j in order:
                     continue
                 d = np.linalg.norm(np.array(c) - np.array(cn))
@@ -197,6 +304,31 @@ class ArucoImageMasker:
         corners, marker_ids, _ = cv2.aruco.detectMarkers(
             gray_img, self.aruco_dictionary, parameters=self.aruco_parameters)
         return corners, marker_ids
+    
+    def compute_marker_poses(self, corners):
+        if cv2.__version__ > '4.0.0':
+            rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(
+                corners, self.marker_size, self.intrinsic_mat, self.distortion)
+        else:
+            rvecs, tvecs = cv2.aruco.estimatePoseSingleMarkers(
+                corners, self.marker_size, self.intrinsic_mat, self.distortion)
+            
+        poses = []
+        for (tv, rv) in zip(tvecs, rvecs):
+            pose = Pose()
+            pose.position.x = tv[0][0]
+            pose.position.y = tv[0][1]
+            pose.position.z = tv[0][2]
+
+            rot_matrix = cv2.Rodrigues(np.array(rv[0]))[0]
+            quat = qtn.from_rotation_matrix(rot_matrix)
+
+            pose.orientation.x = quat.x
+            pose.orientation.y = quat.y
+            pose.orientation.z = quat.z
+            pose.orientation.w = quat.w
+            poses.append(pose)
+        return poses
 
 
 class MaskColorAndDepthImage(rclpy.node.Node):
@@ -228,7 +360,7 @@ class MaskColorAndDepthImage(rclpy.node.Node):
         image_topic = self.get_parameter("image_topic").get_parameter_value().string_value
         rgb_info_topic = self.get_parameter("rgb_info_topic").get_parameter_value().string_value
         depth_image_topic = self.get_parameter("depth_image_topic").get_parameter_value().string_value
-        depth_info_topic = self.get_parameter("rgb_info_topic").get_parameter_value().string_value
+        depth_info_topic = self.get_parameter("depth_info_topic").get_parameter_value().string_value
         self.camera_frame = self.get_parameter("camera_frame").get_parameter_value().string_value
         self.armarker_info_topic = self.get_parameter("marker_info_topic").get_parameter_value().string_value
 
@@ -408,9 +540,12 @@ class MaskColorAndDepthImage(rclpy.node.Node):
     
     def update_masked_img(self, id):
         img_result = self.img_masker.get_masked_img(
-            self.rgb_img, self.depth_img, id, False)
+            self.marker_size, self.intrinsic_mat, self.distortion,
+            self.rgb_img, self.depth_img, id, False, get_seg_mask=False)
+
         corners_result = self.img_masker.get_corners(id)
-        if img_result is None or corners_result is None:
+        if corners_result is None:
+            print('corners_result is None')
             return
         depth_img = img_result[1]
         mask_corners, _, corners_depth = corners_result
@@ -420,10 +555,18 @@ class MaskColorAndDepthImage(rclpy.node.Node):
         for row in img:
             for x in row:
                 if 20 < x < max_z - 100:
+                    print('something higher')
                     return
-                
+
+        img_result = self.img_masker.get_masked_img(
+            self.marker_size, self.intrinsic_mat, self.distortion,
+            self.rgb_img, self.depth_img, id, True, get_seg_mask=True)
+        if img_result is None:
+            print('img_result is None!')
+            return
+        print('Got img result')
         self.img_result[id] = img_result
-        self.corners_result[id] = corners_result
+        self.corners_result[id] = self.img_masker.get_corners(id)
         return
         
     def image_masking_cb(self, req, res):
@@ -455,6 +598,8 @@ class MaskColorAndDepthImage(rclpy.node.Node):
             poses, _, _ = self.compute_marker_poses(marker_corners)
             res.marker_poses = poses
             res.corners_depth = corners_depth
+            res.poses_frame = self.rgb_img_header.frame_id
+
 
         if req.mode & rq.START_UPDATE_TIMER:
             self.start_update_timer()
@@ -507,6 +652,7 @@ class MaskColorAndDepthImage(rclpy.node.Node):
         res.rgb_img.header = self.rgb_img_header
         res.depth_img.header = self.depth_img_header
         res.marker_poses = poses
+        res.poses_frame = self.rgb_img_header.frame_id
         res.corners_depth = corners_depth
         res.success = True
         return res
